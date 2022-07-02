3785bbf1ffeb608b04fa3d7abcc29e9bcfbd7574
taroyanaka <6591797+taroyanaka@users.noreply.github.com>
2022-07-02 11:20:10 +0900

    any way commit

--- docs/src/App.vue
+++ docs/src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App!"/>
+<h1>{{ foo_data }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// local node.js express.js server setup (with hot reloading)
+// server side fetch test
+
+// sqlite3 database setup
+// fetch with sqlite3(better-sqlite3)
+
+// fetch with authentication test
+// fetch with validation test
+
+// fetch with authentication and validation test
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            foo_data: 'FOO123'
+        }
+    }
 }
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
